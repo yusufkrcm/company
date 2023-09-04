615ec76aad960861aee2fa5a4c91a59214e419aa
yusufkrcm <emyusuf58@hotmail.com>
2023-09-04 17:38:56 +0300

    basic spring feign client successfully added

--- employee/src/main/kotlin/com/company/employee/controller/ApigwDenemeController.kt
+++ employee/src/main/kotlin/com/company/employee/controller/ApigwDenemeController.kt
@@ -1,9 +1,11 @@
 package com.company.employee.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("employee")
 class ApigwDenemeController {
     @GetMapping
     fun getAnything() {
